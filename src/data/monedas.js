1f2926824de9cbdbdf48ba4eb2376f2fc048a185
MartiBlanco <blancomarti87@gmail.com>
2022-01-11 01:23:36 -0300

    Proyecto cotizador de Criptomonedas

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    { id:'USD', nombre:'Dólar Americano'},
+    { id:'ARG', nombre:'Peso Argentino'},
+    { id:'MXN', nombre:'Peso Mexicano'},
+    { id:'EUR', nombre:'Euro'},
+    { id:'UY', nombre:'Peso Uruguayo'},
+   ]
+
+   export{
+       monedas  
+   }
